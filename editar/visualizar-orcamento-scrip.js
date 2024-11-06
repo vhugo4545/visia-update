ee3141632eb26ec24c77f5e5666ffa96bb1196d9
vhugo4545 <vhugo7751@gmail.com>
2024-11-06 18:53:27 -0300

    Descrição das alterações feitas

--- editar/visualizar-orcamento-scrip.js
+++ editar/visualizar-orcamento-scrip.js
@@ -1308,7 +1308,7 @@ async function gerarEEnviarProposta() {
         },
         informacoes_adicionais: {
             codigo_categoria: "1.05.98",
-            codigo_conta_corrente: 3498195819,
+            codigo_conta_corrente: 8470223564,
             consumidor_final: "S",
             enviar_email: "N"
         },
